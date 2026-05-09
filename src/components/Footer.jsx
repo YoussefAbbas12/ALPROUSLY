faeb53162191fcaf1e57b5989a2062921862afbf
Youssef Abbas <156310314+YoussefAbbas12@users.noreply.github.com>
2026-05-09 15:50:43 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -128,7 +128,7 @@ export function Footer() {
 
         {/* Bottom */}
         <div className="pt-12 border-t border-white/5 flex flex-col md:flex-row justify-between items-center gap-6">
-          <p className="text-slate-600 text-xs font-medium uppercase tracking-widest">
+          <p className="text-slate-600 text-xs font-medium tracking-widest">
             © {currentYear} InfernoDevs Team. ALL RIGHTS RESERVED.
           </p>
         </div>
